6333f387bb98c890544a17d6e70d05d8730cca18
James <jamescamerontaylor20@gmail.com>
2024-05-28 15:56:41 +1000

    filled in the seeds

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES ('Management'),
+       ('Human Resources'),
+       ('Accounting'),
+       ('IT');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('General Manager', 140000, 1),
+       ('Human Resource Officer' 100000, 2),
+       ('Accountant', 110000, 3),
+       ('IT Worker', 105000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Jones', 1,),
+       ('Peter', 'Moilet', 2, 1),
+       ('Julia', 'Taylor', 3, 1),
+       ('Gerard', 'Timp', 4, 1); 
